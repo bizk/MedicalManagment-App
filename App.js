ee6773b65b46eff838e22d3336108fc41b4b93f0
Carlos Santiago Yanzon <27785807+bizk@users.noreply.github.com>
2020-04-17 21:49:09 -0300

    Init

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Text } from '@ui-kitten/components';
+
+export default function App() {
+  return (
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <HomeScreen />
+    </ApplicationProvider>
+  );
+}
+
+const HomeScreen = () => (
+  <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+    <Text category='h1'>HOME</Text>
+  </Layout>
+);
